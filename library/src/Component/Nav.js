86c14834c77205a7d01021ef55f3a3f394a02a8c
monicakrishb <monica.krish@aspiresys.com>
2023-01-31 17:38:06 +0530

    modified routings

--- library/src/Component/Nav.js
+++ library/src/Component/Nav.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom"
 import { isAuthenticated } from "../services/Auth"
 import './Nav.css';
 function Nav(props) {
-
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             {/* <a className="navbar-brand" href="#">Library</a> */}
@@ -17,16 +16,19 @@ function Nav(props) {
                     {!isAuthenticated() ? <li><Link className="nav-link login" to="/login" >Login</Link></li> : null}
                     {isAuthenticated() ? <li className="nav-item dashboard"><Link className="nav-link" to="/dashboard" >Dashboard</Link></li> : null}
                     {isAuthenticated() ? <li><a className="nav-link logout" href="#" onClick={props.logoutUser} >Logout</a></li> : null}
+                   
+
 
 
                 </ul>
-                <div className='fav'><Link to="/favourites"><h6>Wishlist</h6></Link>
+                <div className='fav'><Link to="/favourites"><h5>Wishlist</h5></Link>
                 </div>
                 <i className="fas fa-heart"></i>
                 <div className='cart '>
-                    <Link to="/booksearch"> <h6>Search</h6></Link>
+                    <Link to="/booksearch"> <h5>Search</h5></Link>
                 </div>
             </div>
+
         </nav>
 
     )
