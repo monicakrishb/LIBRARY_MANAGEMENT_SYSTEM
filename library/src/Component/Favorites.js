fa30879567b6c5609c710c34a14cbaac151566ce
monicakrishb <monica.krish@aspiresys.com>
2023-01-31 17:37:23 +0530

    nav link

--- library/src/Component/Favorites.js
+++ library/src/Component/Favorites.js
@@ -1,38 +1,44 @@
+
 import React from 'react'
 import './App.css';
 import { useAppContext } from './context/appContext';
+import Nav from './Nav';
+
 const Favorites = () => {
-  const   {favourites,addToFavourites,removeFromFavourites}=useAppContext();
-  console.log('favourites are',favourites);
+  const { favourites, addToFavourites, removeFromFavourites } = useAppContext();
+  console.log('favourites are', favourites);
 
-
-  const favouritesChecker=(id)=>{
-    const boolean=favourites.some((book)=>book.id===id);
+  const favouritesChecker = (id) => {
+    const boolean = favourites.some((book) => book.id === id);
     return boolean;
   };
 
   return (
-    <div id='favourite' className='favourites'>
-       {favourites.length>0?favourites.map((book)=>(
-        <div key={book.id}className="book">
+    <div>
+      {/* <Nav/> */}
+    <div className='favourites'>
+      {favourites.length > 0 ? favourites.map((book) => (
+        <div key={book.id} className="book">
           <div> <h4>{book.title}</h4></div>
-          <div><img src={book.image_url}alt="#"/></div>
+          <div><img className="wish" id="wishlist" src={book.image_url} alt="#" /></div>
           <div>
-            {favouritesChecker(book.id)?(
-              <button onClick={()=>removeFromFavourites(book.id)}>
-              Remove from favourites
+            {favouritesChecker(book.id) ? (
+              <button onClick={() => removeFromFavourites(book.id)}>
+                Remove from favourites </button>
+            ) : (
+              <button onClick={() => addToFavourites(book)}>
+                Add to Favourites
               </button>
-              ):(
-            <button onClick={()=>addToFavourites(book)}>
-              Add to Favourites
-              </button>
-)}
-        </div>
-        </div>
-      
-      )):<h1>you dont have any favourite book yet</h1>}
+            )}
+          </div> </div>
+
+      )) : <h5>you dont have any favourite book yet</h5>}
+    </div>
+    <br/>
+    <br/>
     </div>
   );
 }
 
 export default Favorites
+
