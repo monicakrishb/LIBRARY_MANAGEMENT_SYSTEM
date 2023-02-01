931d077267bc033129c5059c0070c87a48f6adca
monicakrishb <monica.krish@aspiresys.com>
2023-02-01 17:43:22 +0530

    api

--- library/src/Component/API.js
+++ library/src/Component/API.js
@@ -1,6 +1,6 @@
 // export const API_URL='https://example-data.draftbit.com/books?_limit=10'
 export const API_URL="http://localhost:4687/book";
-export const BOOK_DETAILS_URL="http://localhost:3000/book/12";
-export const CART_URL="http://localhost:4687/list";
+
+
 
 
